d6bfd869d3ebf55d71684e01bb9f3ba628e080e4
Stefano Simoni <stefano.simoni.10@hotmail.com.ar>
2024-05-04 16:34:56 -0300

    Commit 1
    Components cardModal and card, funcionality to open and close modal and list of cards based in the quantity.

---
+++ assets/scripts/card.js
@@ -0,0 +1,60 @@
+const cardList = [];
+
+// Se inserta Card en index.html
+const getCard = () => {
+  return fetch("../components/card.html")
+    .then((response) => response.text())
+    .then((html) => {
+      return html;
+    })
+    .catch((err) => console.error("Failed to load card: ", err));
+};
+
+for (let i = 0; i < 10; i++) {
+  getCard().then((html) => {
+    document.getElementById("card-container").innerHTML += html;
+    document.getElementById("card").setAttribute("id", "card" + i);
+    document
+      .getElementById("buttonOpenModal")
+      .setAttribute("id", "buttonOpenModal" + i);
+    document.getElementById("cardTitle").setAttribute("id", "cardTitle" + i);
+    document.getElementById("cardTitle" + i).innerHTML += i;
+    cardList.push(html);
+  });
+}
+
+// Se inserta Modal en index.html
+const getCardModal = () => {
+  return fetch("../components/cardModal.html")
+    .then((response) => response.text())
+    .then((html) => {
+      return html;
+    })
+    .catch((err) => console.error("Failed to load modal: ", err));
+};
+
+// Funcionalidades del Modal
+getCardModal().then((html) => {
+  document.querySelector("main").innerHTML += html;
+  const cardModal = document.getElementById("cardModal");
+  cardModal.style.visibility = "hidden";
+
+  // Funcionalidad para mostrar el Modal
+  getCard().then(() => {
+    for (let i = 0; i < cardList.length; i++) {
+      const buttonOpenModal = document.getElementById("buttonOpenModal" + i);
+      buttonOpenModal.addEventListener("click", () => {
+        cardModal.style.visibility = "visible";
+        cardModal.showModal();
+      });
+    }
+  });
+
+  // Funcionalidad para cerrar el Modal
+  cardModal.addEventListener("click", (e) => {
+    if (e.target.id === "cardModal") {
+      cardModal.close();
+      cardModal.style.visibility = "hidden";
+    }
+  });
+});
