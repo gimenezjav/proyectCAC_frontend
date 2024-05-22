cf72d268c0217d6c5d2a8be9e4c98b26fa5d1e2c
Gimenez Javier <gimenez.javier.a@gmail.com>
2024-05-22 09:27:20 -0300

    media query con 3 cortes

--- assets/scripts/inicio.js
+++ assets/scripts/inicio.js
@@ -5,6 +5,3 @@ function openNav() {
 function closeNav() {
     document.getElementById("custom-sidebar").style.width = "0";
 }
-
-// Evento para el botón del menú hamburguesa
-document.getElementById('sidebarToggle').addEventListener('click', openNav);
